843fca9e753435219ca6e114302b2682f98c2070
John Chang <tschangc@gmail.com>
2015-09-25 17:54:14 +0800

    add ingredients and directions

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'paperclip', '~> 4.2.0'
 gem 'cocoon', '~> 1.2.6'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem "devise"
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -47,7 +47,7 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:title, :description, :image)
+    params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
   end
 
 
